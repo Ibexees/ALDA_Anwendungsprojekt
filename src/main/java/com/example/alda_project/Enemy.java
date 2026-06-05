3dcf60b7bd6acf954ce13d535831808f415b8f9e
Marius <marius.marik.19@gmail.com>
2026-06-05 21:55:03 +0200

    feat: selection sort for enemies

--- src/main/java/com/example/alda_project/Enemy.java
+++ src/main/java/com/example/alda_project/Enemy.java
@@ -13,4 +13,30 @@ public class Enemy {
 
     }
 
+    public static void selectionSort(Enemy[] enemies) {
+
+        int indexSmallestUnsorted;
+        for (int i = 0; i < enemies.length; i++)
+        {
+            //leftmost counts as sorted initially
+            indexSmallestUnsorted = i;
+
+            for (int j = i+1; j < enemies.length; j++)
+            {
+                if(enemies[indexSmallestUnsorted].speed > enemies[j].speed)
+                {
+                    indexSmallestUnsorted = j;
+                }
+            }
+            if(indexSmallestUnsorted > i)
+            {
+                Enemy placeholder = enemies[indexSmallestUnsorted];
+                enemies[indexSmallestUnsorted] = enemies[i];
+                enemies[i] = placeholder;
+            }
+
+        }
+
+    }
+
 }
